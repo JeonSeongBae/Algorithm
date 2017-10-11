157d074ccb78e5b8d7cfdcf67a9611adbaf73bcc
JeonSeongBae <tjdqo6042@naver.com>
2017-10-11 16:07:46 +0900

    Invariant_fileReadAndBinarySearch

--- [Algo]homework03/src/LoopInvariant.java
+++ [Algo]homework03/src/LoopInvariant.java
@@ -1,117 +1,99 @@
 import java.io.BufferedReader;
 import java.io.FileInputStream;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Scanner;
 import java.util.StringTokenizer;
 
+import javax.crypto.SealedObject;
+
 public class LoopInvariant {
 
 	public static void main(String[] args) throws IOException {
-		// 초기조건 (Initialization)
-		// 유지 조건 (Maintenance)
-		// 종료 조건 (Termination)
 		LoopInvariant li = new LoopInvariant();
-		int[][] initA = new int[10000][2];
-
-		String fp = "closest+data\\closest_data.txt";
+		Scanner scanner = new Scanner(System.in);
+		String fp = "invariant_data.txt";
 		FileInputStream fis = new FileInputStream(fp);
 		InputStreamReader isr = new InputStreamReader(fis, "euc-kr");
 		BufferedReader br = new BufferedReader(isr);
-		String line = br.readLine();
-		int size = 0;
-		while (line != null) {
-			StringTokenizer st = new StringTokenizer(line, " ");
-			initA[size][0] = Integer.parseInt(st.nextToken()); // x 좌표 저장
-			initA[size++][1] = Integer.parseInt(st.nextToken()); // y 좌표 저장
-			line = br.readLine();
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+		int count = 0;
+		while (st.hasMoreTokens()) {
+			count++;
+			st.nextToken();
 		}
-		int[][] A = new int[size][2];
-		System.arraycopy(initA, 0, A, 0, size); // 사용할 배열의 크기만큼 생성
-
-		int[][] sortA = new int[size][2]; // merge소트를 하기위한 정렬된 배열
-		li.mergeSort(A, sortA, 0, size - 1);
-		// System.out.println("찾고자 하는 수 x는 표준입력을 사용하여 직접 입력");
-		// int find = Scanner.nextInt();
-
-		double leftmin = li.leftDistance(A, 0, 1,
-				Math.sqrt(Math.pow((A[1][0] - A[0][0]), 2) + Math.pow((A[1][1] - A[0][1]), 2)));
-		double rightmin = li.rightDistance(A, A.length / 2, (A.length / 2) + 1,
-				Math.sqrt(Math.pow((A[A.length - 1][0] - A[A.length - 2][0]), 2)
-						+ Math.pow((A[A.length - 1][1] - A[A.length - 2][1]), 2)));
+		int[] A = new int[count];
+		int[] sortedA = new int[count];
+		fis = new FileInputStream(fp);
+		isr = new InputStreamReader(fis, "euc-kr");
+		br = new BufferedReader(isr);
+		st = new StringTokenizer(br.readLine(), " ");
+		// System.out.println("파일 읽기 시작");
+		for (int i = 0; st.hasMoreTokens(); i++) {
+			A[i] = Integer.parseInt(st.nextToken());
+		}
+		li.mergeSort(A, sortedA, 0, A.length - 1);
+		System.out.println("찾고자 하는 수 x를 입력하세요.");
+		int x = scanner.nextInt();
+		li.Search(A, A.length, x);
 	}
 
-	private double leftDistance(int[][] A, int i, int j, double min) {
-		double distance = Math.sqrt(Math.pow((A[i][0] - A[j][0]), 2) + Math.pow((A[i][1] - A[j][1]), 2));
-		if (min > distance) {
-			min = distance;
+	int Search(int A[], int length, int x) {
+
+		// 초기조건 (Initialization)
+		// 유지 조건 (Maintenance)
+		// 종료 조건 (Termination)
+		int left = 0, right = length - 1, middle;
+
+		while (left < right) {
+			middle = (left + right) / 2;
+			if (A[middle] < x)
+				left = middle + 1;
+			else if (A[middle] > x)
+				right = middle - 1;
+			else
+				return middle;
 		}
-		if (i >= (A.length / 2) - 1) {
-			return min;
-		} else if (j >= (A.length / 2) - 1) {
-			leftDistance(A, ++i, i + 1, min);
-		} else {
-			leftDistance(A, i, ++j, min);
-		}
-		return min;
+		return -1;
 	}
 
-	private double rightDistance(int[][] A, int i, int j, double min) {
-		double distance = Math.sqrt(Math.pow((A[i][0] - A[j][0]), 2) + Math.pow((A[i][1] - A[j][1]), 2));
-		if (min > distance) {
-			min = distance;
-		}
-		if (i >= A.length - 2) {
-			return min;
-		} else if (j >= A.length - 1) {
-			rightDistance(A, ++i, i + 1, min);
-		} else {
-			rightDistance(A, i, ++j, min);
-		}
-		return min;
-	}
-
-	private void mergeSort(int[][] a, int[][] sortA, int left, int right) {
+	public static void mergeSort(int[] A, int[] sortedA, int left, int right) {
 		int middle;
 		if (left < right) {
 			middle = (left + right) / 2;
-			mergeSort(a, sortA, left, middle);
-			mergeSort(a, sortA, middle + 1, right);
-			merge(a, sortA, left, middle, right);
+			mergeSort(A, sortedA, left, middle);
+			mergeSort(A, sortedA, middle + 1, right);
+			merge(A, sortedA, left, middle, right);
 		}
 	}
 
-	private void merge(int[][] a, int[][] sortA, int left, int middle, int right) {
+	public static void merge(int[] arr, int[] sorted, int left, int middle, int right) {
 		int i = left;
 		int j = middle + 1;
 		int k = left;
 		int t;
 
 		while (i <= middle && j <= right) {
-			if (a[i][0] <= a[j][0]) {
-				sortA[k][0] = a[i][0];
-				sortA[k][1] = a[i++][1];
+			if (arr[i] <= arr[j]) {
+				sorted[k] = arr[i++];
 			} else {
-				sortA[k][0] = a[j][0];
-				sortA[k][1] = a[j++][1];
+				sorted[k] = arr[j++];
 			}
 			k++;
 		}
 		if (i > middle) {
 			for (t = j; t <= right; t++, k++) {
-				sortA[k][0] = a[t][0];
-				sortA[k][1] = a[t][1];
+				sorted[k] = arr[t];
 			}
 		} else {
 			for (t = i; t <= middle; t++, k++) {
-				sortA[k][0] = a[t][0];
-				sortA[k][1] = a[t][1];
+				sorted[k] = arr[t];
 			}
 		}
 		for (t = left; t <= right; t++) {
-			a[t][0] = sortA[t][0];
-			a[t][1] = sortA[t][1];
+			arr[t] = sorted[t];
 		}
 	}
-
 }
