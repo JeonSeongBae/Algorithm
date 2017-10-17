c06a973c7b86f44784b8a4945c22383666700a30
JeonSeongBae <tjdqo6042@naver.com>
2017-10-17 14:25:03 +0900

    readKaratsubaAlgorithmAndsavebigIntarray

--- [Algo]homework04/src/CountingInversions.java
+++ [Algo]homework04/src/CountingInversions.java
@@ -10,18 +10,22 @@ public class CountingInversions {
 		CountingInversions self = new CountingInversions();
 		for (int i = 1; i < 5; i++) {
 			String fp = "data/data05_inversion_0" + i + ".txt";
+			// Count the number of tokens to determine the size of the array
 			int count = self.arrayCounting(fp);
+			// make an array of the number of tokens
 			int[] A = new int[count];
+			/* 파일 읽어오기 */
 			FileInputStream fis = new FileInputStream(fp);
 			InputStreamReader isr = new InputStreamReader(fis, "euc-kr");
 			BufferedReader br = new BufferedReader(isr);
 
 			String line = br.readLine();
 			StringTokenizer st = new StringTokenizer(line, " ");
+			// save data in a file in an array
 			while (line != null) {
 				st = new StringTokenizer(line, " ");
 				for (int j = 0; st.hasMoreTokens(); j++) {
-					A[j] = Integer.parseInt(st.nextToken()); // x 좌표 저장
+					A[j] = Integer.parseInt(st.nextToken());
 				}
 				line = br.readLine();
 			}
@@ -29,6 +33,7 @@ public class CountingInversions {
 		}
 	}
 
+	// A function that counts the token of a file
 	private int arrayCounting(String fp) throws IOException {
 		// TODO Auto-generated method stub
 		FileInputStream fis = new FileInputStream(fp);
@@ -48,6 +53,7 @@ public class CountingInversions {
 	}
 
 	private int SORT_AND_COUNT(int[] L) {
+		// if size is 1 return 0 and L
 		if (L.length == 1) {
 			return 0;
 		}
@@ -61,6 +67,7 @@ public class CountingInversions {
 		int ra = SORT_AND_COUNT(left);
 		int rb = SORT_AND_COUNT(right);
 		int r = MERGE_AND_COUNT(L, left, right);
+		// return sum of count and sorted L
 		return ra + rb + r;
 	}
 
@@ -77,12 +84,13 @@ public class CountingInversions {
 				L[indexL++] = left[indexA++];
 			}
 		}
+		// save remaining elements to list
 		if (indexA <= left.length - 1) {
 			System.arraycopy(left, indexA, L, indexL, L.length - indexL - 1);
 		} else {
 			System.arraycopy(right, indexB, L, indexL, L.length - indexL - 1);
 		}
-
+		// return inverstion count
 		return inverstion_count;
 	}
 
--- [Algo]homework04/src/KaratsubaAlgorithm.java
+++ [Algo]homework04/src/KaratsubaAlgorithm.java
@@ -8,20 +8,43 @@ public class KaratsubaAlgorithm {
 
 	public static void main(String[] args) throws IOException {
 		KaratsubaAlgorithm self = new KaratsubaAlgorithm();
-
 		String fp = "data/data05_karatsuba.txt";
-		int count = self.arrayCounting(fp);
-		int[] A = new int[count];
+		// Count the number of tokens to determine the size of the array
 		FileInputStream fis = new FileInputStream(fp);
 		InputStreamReader isr = new InputStreamReader(fis, "euc-kr");
 		BufferedReader br = new BufferedReader(isr);
-
+		int count = 0;
 		String line = br.readLine();
+		int bigIntA = Integer.parseInt(line);
+		int lengthA = (int) (Math.log10(bigIntA) + 1);
+		int[] A = new int[lengthA];
+		for (int i = lengthA - 1; bigIntA % 10 != 0 || bigIntA / 10 != 0; i--) {
+			A[i] = bigIntA % 10;
+			bigIntA /= 10;
+		}
+
+		line = br.readLine();
+		int bigIntB = Integer.parseInt(line);
+		int lengthB = (int) (Math.log10(bigIntB) + 1);
+		int[] B = new int[lengthB];
+		for (int i = lengthB - 1; bigIntB % 10 != 0 || bigIntB / 10 != 0; i--) {
+			A[i] = bigIntB % 10;
+			bigIntB /= 10;
+		}
+
+		// make an array of the number of tokens
+		/* 파일 읽어오기 */
+		fis = new FileInputStream(fp);
+		isr = new InputStreamReader(fis, "euc-kr");
+		br = new BufferedReader(isr);
+
+		line = br.readLine();
 		StringTokenizer st = new StringTokenizer(line, " ");
+		// save data in a file in an array
 		while (line != null) {
 			st = new StringTokenizer(line, " ");
-			for (int i = 0; st.hasMoreTokens(); i++) {
-				A[i] = Integer.parseInt(st.nextToken()); // x 좌표 저장
+			for (int j = 0; st.hasMoreTokens(); j++) {
+				A[j] = Integer.parseInt(st.nextToken());
 			}
 			line = br.readLine();
 		}
@@ -34,16 +57,16 @@ public class KaratsubaAlgorithm {
 		BufferedReader br = new BufferedReader(isr);
 		int count = 0;
 		String line = br.readLine();
-		StringTokenizer st = new StringTokenizer(line, " ");
-		while (line != null) {
-			while (st.hasMoreTokens()) {
-				count++;
-				st.nextToken();
-			}
-			line = br.readLine();
+		int bigInt = Integer.parseInt(line);
+		while (bigInt % 10 != 0 && bigInt / 10 != 0) {
+
+		}
+		StringTokenizer st = new StringTokenizer(line, "");
+		while (st.hasMoreTokens()) {
+			count++;
+			st.nextToken();
 		}
 		return count;
 	}
-
 
 }
