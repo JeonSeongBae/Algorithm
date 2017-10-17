3bc997f80b654d9660486aa7c2f711c27249caa1
JeonSeongBae <tjdqo6042@naver.com>
2017-10-17 14:28:22 +0900

    writeComent

--- [Algo]homework04/src/KaratsubaAlgorithm.java
+++ [Algo]homework04/src/KaratsubaAlgorithm.java
@@ -9,64 +9,36 @@ public class KaratsubaAlgorithm {
 	public static void main(String[] args) throws IOException {
 		KaratsubaAlgorithm self = new KaratsubaAlgorithm();
 		String fp = "data/data05_karatsuba.txt";
+
 		// Count the number of tokens to determine the size of the array
 		FileInputStream fis = new FileInputStream(fp);
 		InputStreamReader isr = new InputStreamReader(fis, "euc-kr");
 		BufferedReader br = new BufferedReader(isr);
-		int count = 0;
 		String line = br.readLine();
+
+		// make first array
 		int bigIntA = Integer.parseInt(line);
 		int lengthA = (int) (Math.log10(bigIntA) + 1);
 		int[] A = new int[lengthA];
+
+		// save data in a file in an array
 		for (int i = lengthA - 1; bigIntA % 10 != 0 || bigIntA / 10 != 0; i--) {
 			A[i] = bigIntA % 10;
 			bigIntA /= 10;
 		}
 
+		// make second array
 		line = br.readLine();
 		int bigIntB = Integer.parseInt(line);
 		int lengthB = (int) (Math.log10(bigIntB) + 1);
 		int[] B = new int[lengthB];
+
+		// save data in a file in an array
 		for (int i = lengthB - 1; bigIntB % 10 != 0 || bigIntB / 10 != 0; i--) {
-			A[i] = bigIntB % 10;
+			B[i] = bigIntB % 10;
 			bigIntB /= 10;
 		}
 
-		// make an array of the number of tokens
-		/* 파일 읽어오기 */
-		fis = new FileInputStream(fp);
-		isr = new InputStreamReader(fis, "euc-kr");
-		br = new BufferedReader(isr);
-
-		line = br.readLine();
-		StringTokenizer st = new StringTokenizer(line, " ");
-		// save data in a file in an array
-		while (line != null) {
-			st = new StringTokenizer(line, " ");
-			for (int j = 0; st.hasMoreTokens(); j++) {
-				A[j] = Integer.parseInt(st.nextToken());
-			}
-			line = br.readLine();
-		}
-	}
-
-	private int arrayCounting(String fp) throws IOException {
-		// TODO Auto-generated method stub
-		FileInputStream fis = new FileInputStream(fp);
-		InputStreamReader isr = new InputStreamReader(fis, "euc-kr");
-		BufferedReader br = new BufferedReader(isr);
-		int count = 0;
-		String line = br.readLine();
-		int bigInt = Integer.parseInt(line);
-		while (bigInt % 10 != 0 && bigInt / 10 != 0) {
-
-		}
-		StringTokenizer st = new StringTokenizer(line, "");
-		while (st.hasMoreTokens()) {
-			count++;
-			st.nextToken();
-		}
-		return count;
 	}
 
 }
