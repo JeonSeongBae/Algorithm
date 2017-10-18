d125aa2ef17293e6970f9280fdae4b8aa7e7e625
JeonSeongBae <tjdqo6042@naver.com>
2017-10-18 09:58:11 +0900

    KaratsubaAlgorithm_second_divide_number_incorrect

--- [Algo]homework04/src/KaratsubaAlgorithm.java
+++ [Algo]homework04/src/KaratsubaAlgorithm.java
@@ -35,22 +35,22 @@ public class KaratsubaAlgorithm {
 		}
 		// Divide A and store data in X2 and X0
 		BigInteger X2 = new BigInteger(String.valueOf(A.intValue()).substring(0, lengthA / 2));
-		BigInteger Y2 = new BigInteger(String.valueOf(A.intValue()).substring(lengthA / 2));
+		BigInteger X1 = new BigInteger(String.valueOf(A.intValue()).substring(lengthA / 2));
 
 		// Divide B and store data in Y2 and Y0
-		BigInteger X1 = new BigInteger(String.valueOf(B.intValue()).substring(0, lengthB / 2));
+		BigInteger Y2 = new BigInteger(String.valueOf(B.intValue()).substring(0, lengthB / 2));
 		BigInteger Y1 = new BigInteger(String.valueOf(B.intValue()).substring(lengthB / 2));
 
-		BigInteger z2 = KARATSUBA(X2, Y2);
-		BigInteger z0 = KARATSUBA(X1, Y1);
+		BigInteger z0 = KARATSUBA(X2, Y2);
+		BigInteger z2 = KARATSUBA(X1, Y1);
 		BigInteger z1 = KARATSUBA((X2.add(X1)), (Y2.add(Y1)));
-		z1 = z1.subtract(z0).subtract(z2);
+		z1 = z1.subtract(z2).subtract(z0);
 
 		BigInteger temp = new BigInteger(String.valueOf((int)Math.pow(10, lengthA / 2 * 2)));
-		z2 = z2.multiply(temp);
+		z0 = z0.multiply(temp);
 		temp = new BigInteger(String.valueOf((int)Math.pow(10, lengthA / 2)));
 		z1 = z1.multiply(temp);
 
-		return z2.add(z1).add(z0);
+		return z0.add(z1).add(z2);
 	}
 }
