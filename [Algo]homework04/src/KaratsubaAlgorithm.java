cf7ca8c5bfb6a02b90178dd01f23929a72c81f43
JeonSeongBae <tjdqo6042@naver.com>
2017-10-18 09:20:16 +0900

    Karatsuba_resulterror

--- [Algo]homework04/src/KaratsubaAlgorithm.java
+++ [Algo]homework04/src/KaratsubaAlgorithm.java
@@ -2,6 +2,7 @@ import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.math.BigInteger;
 import java.util.StringTokenizer;
 
 public class KaratsubaAlgorithm {
@@ -14,31 +15,42 @@ public class KaratsubaAlgorithm {
 		FileInputStream fis = new FileInputStream(fp);
 		InputStreamReader isr = new InputStreamReader(fis, "euc-kr");
 		BufferedReader br = new BufferedReader(isr);
-		String line = br.readLine();
 
 		// make first array
-		int bigIntA = Integer.parseInt(line);
-		int lengthA = (int) (Math.log10(bigIntA) + 1);
-		int[] A = new int[lengthA];
-
-		// save data in a file in an array
-		for (int i = lengthA - 1; bigIntA % 10 != 0 || bigIntA / 10 != 0; i--) {
-			A[i] = bigIntA % 10;
-			bigIntA /= 10;
-		}
+		String line = br.readLine();
+		BigInteger bigA = new BigInteger(line);
 
 		// make second array
 		line = br.readLine();
-		int bigIntB = Integer.parseInt(line);
-		int lengthB = (int) (Math.log10(bigIntB) + 1);
-		int[] B = new int[lengthB];
-
-		// save data in a file in an array
-		for (int i = lengthB - 1; bigIntB % 10 != 0 || bigIntB / 10 != 0; i--) {
-			B[i] = bigIntB % 10;
-			bigIntB /= 10;
-		}
+		BigInteger bigB = new BigInteger(line);
+		System.out.println(self.KARATSUBA(bigA, bigB));
 
 	}
 
+	private BigInteger KARATSUBA(BigInteger A, BigInteger B) {
+		int lengthA = String.valueOf(A.longValue()).length();
+		int lengthB = String.valueOf(B.longValue()).length();
+		if (lengthA <= 2 || lengthB <= 2) {
+			return A.multiply(B);
+		}
+		// Divide A and store data in X2 and X0
+		BigInteger X2 = new BigInteger(String.valueOf(A.intValue()).substring(0, lengthA / 2));
+		BigInteger Y2 = new BigInteger(String.valueOf(A.intValue()).substring(lengthA / 2));
+
+		// Divide B and store data in Y2 and Y0
+		BigInteger X1 = new BigInteger(String.valueOf(B.intValue()).substring(0, lengthB / 2));
+		BigInteger Y1 = new BigInteger(String.valueOf(B.intValue()).substring(lengthB / 2));
+
+		BigInteger z2 = KARATSUBA(X2, Y2);
+		BigInteger z0 = KARATSUBA(X1, Y1);
+		BigInteger z1 = KARATSUBA((X2.add(X1)), (Y2.add(Y1)));
+		z1 = z1.subtract(z0).subtract(z2);
+
+		BigInteger temp = new BigInteger(String.valueOf((int)Math.pow(10, lengthA / 2 * 2)));
+		z2 = z2.multiply(temp);
+		temp = new BigInteger(String.valueOf((int)Math.pow(10, lengthA / 2)));
+		z1 = z1.multiply(temp);
+
+		return z2.add(z1).add(z0);
+	}
 }
