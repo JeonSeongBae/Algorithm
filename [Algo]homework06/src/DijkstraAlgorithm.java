27845124c5c5eb9f8bbfa58c2a05dc80d4e6f85c
JeonSeongBae <tjdqo6042@naver.com>
2017-10-26 21:45:59 +0900

    초기값 설정 및 path가 없는 배열에 infinity 저장

--- [Algo]homework06/src/DijkstraAlgorithm.java
+++ [Algo]homework06/src/DijkstraAlgorithm.java
@@ -2,29 +2,32 @@
 public class DijkstraAlgorithm {
 	// 무한대를 표현
 	final static double infinity = Double.POSITIVE_INFINITY;
-	// class Object {
-	// String start;
-	// String end;
-	// int cost;
-	//
-	// Object(String start, String end, int cost) {
-	// this.start = start;
-	// this.end = end;
-	// this.cost = cost;
-	// }
-	// }
+	/*
+	 * public static class path { String start; String end; int cost;
+	 * 
+	 * public path(String start, String end, int cost) { this.start = start;
+	 * this.end = end; this.cost = cost; } }
+	 */
 
 	public static void main(String[] args) {
-		Object[][] path = new Object[5][5];
+
+		int[][] path = new int[5][5];
 		// A = 0 / B = 1 / C = 2 / D = 3 / E = 4
-		path[0][1] = 10;
-		path[0][1] = 10;
-		path[0][1] = 10;
-		path[0][1] = 10;
-		path[0][1] = 10;
-		path[0][1] = 10;
-		path[0][1] = 10;
-		path[0][1] = 10;
+		path[index("A")][index("B")] = 10;
+		path[index("A")][index("C")] = 3;
+		path[index("B")][index("C")] = 1;
+		path[index("B")][index("D")] = 2;
+		path[index("C")][index("B")] = 4;
+		path[index("C")][index("D")] = 8;
+		path[index("D")][index("E")] = 7;
+		path[index("E")][index("D")] = 9;
+		for (int i = 0; i < path.length; i++) {
+			for (int j = 0; j < path.length; j++) {
+				if (path[i][j] == 0) {
+					path[i][j] = (int) infinity;
+				}
+			}
+		}
 	}
 
 	private static int vertex(int index) {
