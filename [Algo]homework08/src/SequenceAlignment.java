763c6d19eb81898f9f8582965252f83768faf054
JeonSeongBae <tjdqo6042@naver.com>
2017-11-19 21:19:31 +0900

    modified variable name and array type

--- [Algo]homework08/src/SequenceAlignment.java
+++ [Algo]homework08/src/SequenceAlignment.java
@@ -1,20 +1,22 @@
+import java.util.Scanner;
 
 public class SequenceAlignment {
 	// δα
 	static int[][] M = new int[10][10];
+	private static Scanner scanner;
+	static int b = 1;
+	static int mismatch = 2;
 
 	public static void main(String[] args) {
-		int m = 0, n = 0;
-		int[] x = new int[10];
-		int[] y = new int[10];
-		int b = 0;
-		int[][] a = new int[10][10];
-
-		int output = SequenceAlignment(m, n, x, y, b, a);
+		scanner = new Scanner(System.in);
+		char[] x = scanner.nextLine().toCharArray();
+		char[] y = scanner.nextLine().toCharArray();
+		int[][] a = new int[x.length][y.length];
+		int output = SequenceAlignment(x.length, y.length, x, y, b, a);
 		System.out.println(output);
 	}
 
-	private static int SequenceAlignment(int m, int n, int[] x, int[] y, int b, int[][] a) {
+	private static int SequenceAlignment(int m, int n, char[] x, char[] y, int b, int[][] a) {
 		for (int i = 0; i <= m; i++) {
 			M[0][i] = i * b;
 		}
@@ -24,7 +26,7 @@ public class SequenceAlignment {
 		for (int i = 1; i <= m; i++) {
 			for (int j = 1; j <= n; j++) {
 				int min = Math.min(a[x[i]][y[i]] + M[i - 1][j - 1], b + M[i - 1][j]);
-				min = Math.min(min, b + M[i][j - 1]);
+				M[i][j] = Math.min(min, b + M[i][j - 1]);
 			}
 		}
 		return M[m][n];
