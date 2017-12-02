bb2c34859136c2b1f4cb44250e7c110fd7b33926
JeonSeongBae <tjdqo6042@naver.com>
2017-12-02 12:56:01 +0900

    add hw09 report

--- [Algo]homework09/src/bellmanFord.java
+++ [Algo]homework09/src/bellmanFord.java
@@ -11,7 +11,6 @@ public class bellmanFord {
 	static int t;
 	static int edgesCount;
 	final static int infinity = Integer.MAX_VALUE;
-	// final static int infinity = 700;
 
 	public static class Node {
 		int u;
@@ -27,7 +26,7 @@ public class bellmanFord {
 
 	public static void main(String[] args) throws IOException {
 
-		String fp = "data10 - 복사본.txt";
+		String fp = "data10.txt";
 		ArrayList<Node> E = fileRead(fp);
 		ArrayList<Integer> d = new ArrayList<>();
 
@@ -37,7 +36,7 @@ public class bellmanFord {
 		}
 		d.set(s, 0);
 		for (int i = 1; i <= d.size() - 1; i++) {
-			boolean update = false;
+//			boolean update = false;
 //			ArrayList<Integer> clone_d = (ArrayList<Integer>) d.clone();
 			for (int j = 0; j < E.size(); j++) {
 				Node tempE = E.get(j);
@@ -61,9 +60,7 @@ public class bellmanFord {
 			}
 		}
 
-		for (int i = 0; i < d.size(); i++) {
-			System.out.print(d.get(i) + " ");
-		}
+		System.out.println("Output : " + d.get(t));
 
 	}
 
